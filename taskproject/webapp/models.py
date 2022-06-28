0f71835beba56c7d9fb6f19082a24eddf00b193b
apple </Kvadrat2020/>
2022-06-27 22:51:39 -0400

    created new migrate

--- taskproject/webapp/models.py
+++ taskproject/webapp/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+STATUS_CHOICES = [('new', 'New'), ('in_progress', 'Processing'), ('done', 'Completed')]
+
 class Article(models.Model):
     project = models.CharField(max_length=50, null=False, blank=False, verbose_name="Zagalovoc")
     author = models.CharField(max_length=50, verbose_name="Author", default="Unknown")
     content = models.TextField(max_length=3000, verbose_name="Content")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new', verbose_name='Status')
 
 
     def __str__(self):
-        return f"{self.pk}, {self.project}, {self.author}"
+        return f"{self.pk}, {self.project}, {self.author}, {self.status}"
 
 
     class Meta:

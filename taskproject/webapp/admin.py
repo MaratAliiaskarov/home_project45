441b1d0783adb2e1cb505a4605bd05321fb55bd8
apple </Kvadrat2020/>
2022-06-27 23:47:02 -0400

    created status and time in article_view

--- taskproject/webapp/admin.py
+++ taskproject/webapp/admin.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 from webapp.models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'project', 'author']
+    list_display = ['id', 'project', 'author', 'status']
     list_display_links = ['project']
     list_filter = ['author']
-    search_fields = ['project', 'content']
+    search_fields = ['project', 'content', 'status']
     fields = ['project', 'author', 'content', 'created_at', 'updated_at']
-    readonly_fields = ['created_at', 'updated_at']
+    readonly_fields = ['status', 'created_at', 'updated_at']
 
 
 admin.site.register(Article, ArticleAdmin)

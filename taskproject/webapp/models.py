dc5f74b69662571692974d586021d12b8ac6f539
apple </Kvadrat2020/>
2022-07-08 15:26:25 -0400

    created models comments and created migrations

--- taskproject/webapp/models.py
+++ taskproject/webapp/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    class Meta:
+        abstract = True
+
+
 
 STATUS_CHOICES = [('new', 'New'), ('in_progress', 'Processing'), ('done', 'Completed')]
 
-class Article(models.Model):
+class Article(BaseModel):
     project = models.CharField(max_length=50, null=False, blank=False, verbose_name="Zagalovoc")
     author = models.CharField(max_length=50, verbose_name="Author", default="Unknown")
     content = models.TextField(max_length=3000, verbose_name="Content")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new', verbose_name='Status')
 
 
@@ -21,3 +27,18 @@ class Article(models.Model):
         db_table = "articles"
         verbose_name = "Project"
         verbose_name_plural = "Projects"
+
+class Comment(BaseModel):
+    text = models.TextField(max_length=400, verbose_name="Comments")
+    author = models.CharField(max_length=40, null=True, blank=True, default="Anonim", verbose_name="Project")
+    article = models.ForeignKey("webapp.Article", on_delete=models.CASCADE, related_name="comments", verbose_name="Project")
+
+
+    def __str__(self):
+        return f"{self.pk}, {self.text}, {self.author}"
+
+
+    class Meta:
+        db_table = "comments"
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"

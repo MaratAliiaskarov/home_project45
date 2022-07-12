c491477f41787c0487be3189f4728d729b147f67
apple </Kvadrat2020/>
2022-07-11 21:38:15 -0400

    created new migration, updated models.py

--- taskproject/webapp/models.py
+++ taskproject/webapp/models.py
@@ -17,7 +17,7 @@ class Article(BaseModel):
     author = models.CharField(max_length=50, verbose_name="Author", default="Unknown")
     content = models.TextField(max_length=3000, verbose_name="Content")
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new', verbose_name='Status')
-
+    tags = models.ManyToManyField("webapp.Tag", related_name="articles", through="webapp.ArticleTag", through_fields=("article", "tag"), blank=True)
 
     def __str__(self):
         return f"{self.pk}, {self.project}, {self.author}, {self.status}"
@@ -42,3 +42,22 @@ class Comment(BaseModel):
         db_table = "comments"
         verbose_name = "Comment"
         verbose_name_plural = "Comments"
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=31, verbose_name="Tag")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "tags"
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+
+class ArticleTag(models.Model):
+    article = models.ForeignKey("webapp.Article", related_name="article_tags", on_delete=models.CASCADE, verbose_name="Project")
+
+    tag = models.ForeignKey("webapp.Tag", related_name="tags_article", on_delete=models.CASCADE,
+                                verbose_name="Tag")
--- taskproject/webapp/urls.py
+++ taskproject/webapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path("articles/add/", create_article, name="create_article"),
     path('article/<int:pk>/', ArticleView.as_view(extra_context={"test": 5}), name="article_view"),
-    path('article/<int:pk>/update', update_article, name="update_article"),
-    path('article/<int:pk>/delete', delete_article, name="delete_article"),
+    path('article/<int:pk>/update/', update_article, name="update_article"),
+    path('article/<int:pk>/delete/', delete_article, name="delete_article"),
 
 ]

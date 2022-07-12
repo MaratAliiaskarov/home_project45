ddebab9d715e0bad7e0bc5b0874f03c39e721ad7
apple </Kvadrat2020/>
2022-07-11 23:21:25 -0400

    export tabl to article_tags, created forms

--- taskproject/webapp/forms.py
+++ taskproject/webapp/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.forms import widgets
+
+from webapp.models import Tag
+
 
 class ArticleForm(forms.Form):
     project = forms.CharField(max_length=50, required=True, label="Project")
     content = forms.CharField(max_length=3000, required=True, label="Content")
-    author = forms.CharField(max_length=50, required=True, label="Author")
+    author = forms.CharField(max_length=50, required=True, label="Author", widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
+
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False, label="Teg")
--- taskproject/webapp/models.py
+++ taskproject/webapp/models.py
@@ -17,7 +17,9 @@ class Article(BaseModel):
     author = models.CharField(max_length=50, verbose_name="Author", default="Unknown")
     content = models.TextField(max_length=3000, verbose_name="Content")
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new', verbose_name='Status')
-    tags = models.ManyToManyField("webapp.Tag", related_name="articles", through="webapp.ArticleTag", through_fields=("article", "tag"), blank=True)
+    # tags_old = models.ManyToManyField("webapp.Tag", related_name="old_articles", through="webapp.ArticleTag", through_fields=("article", "tag"), blank=True)
+
+    tags = models.ManyToManyField("webapp.Tag", related_name="articles", blank=True)
 
     def __str__(self):
         return f"{self.pk}, {self.project}, {self.author}, {self.status}"
@@ -56,8 +58,8 @@ class Tag(BaseModel):
         verbose_name_plural = "Tags"
 
 
-class ArticleTag(models.Model):
-    article = models.ForeignKey("webapp.Article", related_name="article_tags", on_delete=models.CASCADE, verbose_name="Project")
-
-    tag = models.ForeignKey("webapp.Tag", related_name="tags_article", on_delete=models.CASCADE,
-                                verbose_name="Tag")
+# class ArticleTag(models.Model):
+#     article = models.ForeignKey("webapp.Article", related_name="article_tags", on_delete=models.CASCADE, verbose_name="Project")
+#
+#     tag = models.ForeignKey("webapp.Tag", related_name="tags_article", on_delete=models.CASCADE,
+#                                 verbose_name="Tag")

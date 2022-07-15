e9290a3aa729cc1d288ede4343c74b6a4ffaeb5c
apple </Kvadrat2020/>
2022-07-15 11:30:35 -0400

    updates admin.py, created Tags panel

--- taskproject/webapp/admin.py
+++ taskproject/webapp/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from webapp.models import Article, Tag
 
-# Register your models here.
-from webapp.models import Article
+
+class TagInline(admin.TabularInline):
+    model = Article.tags.through
+
+# class PersonAdmin(admin.ModelAdmin):
+#     inlines = [
+#         TagInline,
+#     ]
+#
+# class GroupAdmin(admin.ModelAdmin):
+#     inlines = [
+#         MembershipInline,
+#     ]
+#     exclude = ('members', )
+
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'project', 'author', 'status']
@@ -10,7 +24,14 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ['project', 'content', 'status']
     fields = ['project', 'author', 'content', 'created_at', 'updated_at']
     readonly_fields = ['status', 'created_at', 'updated_at']
+    # filter_horizontal = ['tags']
+    inlines = [TagInline]
 
 
+class TagsAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
+
+
+admin.site.register(Tag, TagsAdmin)
 admin.site.register(Article, ArticleAdmin)
 
